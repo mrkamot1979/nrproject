36de030800780c67ab653f95885ada2173aa6a68
nenu <nino_roldan@yahoo.com>
2018-01-24 21:27:17 +0900

    Detailed View construction

---
+++ detailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  detailViewController.swift
+//  NRTodo
+//
+//  Created by Nino Rorudan on 1/24/18.
+//  Copyright © 2018 The NTMC Foundation. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIViewController {
+
+    
+    @IBOutlet weak var txtTitle: UILabel!
+    @IBOutlet weak var txtSubtitle: UILabel!
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
